313a99a8efc36aca03f34850a7986bfc9dd81058
Jh_linux <wpghks7@naver.com>
2019-03-12 20:48:15 +0900

    20190312

---
+++ code/06581_HTML.py
@@ -0,0 +1,27 @@
+try:
+    word = 0
+    while True:
+        s = input().split()
+        for x in s:
+            if x == '<br>':
+                print()
+                word = 0
+            elif x == '<hr>':
+                if word == 0:
+                    print('-'*80)
+                else:
+                    print()
+                    print('-' * 80)
+                word = 0
+            else:
+                if word != 0:
+                    word += 1
+                word += len(x)
+                if word > 80:
+                    print()
+                    print(x, end=" ")
+                    word = len(x)
+                else:
+                    print(x, end=" ")
+except:
+    exit()
