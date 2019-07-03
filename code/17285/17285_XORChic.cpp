28e65c28594985be62c8f63baedcf2fe37178e55
LeeJehwan <wpghks7@naver.com>
2019-07-04 02:36:12 +0900

    XORChic

--- code/17285/17285_XORChic.cpp
+++ code/17285/17285_XORChic.cpp
@@ -1,22 +1,12 @@
 #include <iostream>
-#include <vector>
 #include <string>
-#include <algorithm>
 using namespace std;
-
 
 int main() {
 	string s;
 	cin >> s;
-	char ch = s[0];
-	int key = 0;
-	while (1) {
-		if ((key^ch) == 'C')
-			break;
-		key++;
-	}
+	int key = (s[0] ^ 'C');
 	for (int i = 0; i < s.size(); i++)
 		s[i] ^= key;
 	cout << s << endl;
-
 }
